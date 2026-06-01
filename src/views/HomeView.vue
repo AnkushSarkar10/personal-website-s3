efedb636aaa6fa29c08a9ced1f950a0239e631bf
ankush sarkar <ankush.sarkar2002@gmail.com>
2026-06-01 17:36:39 -0400

    new logo stuuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="flex items-center justify-center">
+  <div class="flex flex-col items-center justify-center">
+    <img
+      src="@/assets/@.png"
+      alt=""
+      class="hidden md:block w-16 h-16 object-contain"
+    />
     <h1 class="text-[clamp(3rem,3vw,6rem)] font-bold -tracking-wide text-text select-none">
-      kush<span class="inline-block text-accent animate-pulse">&nbsp;.</span>
+      ankush<span class="inline-block text-accent animate-pulse">&nbsp;.</span>
     </h1>
   </div>
 </template>
