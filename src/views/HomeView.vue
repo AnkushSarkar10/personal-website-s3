87afec70404699fe13c7d600e8e9efbf1e1b6432
ankush sarkar <ankush.sarkar2002@gmail.com>
2026-06-01 17:44:18 -0400

    blog button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,5 +8,11 @@
     <h1 class="text-[clamp(3rem,3vw,6rem)] font-bold -tracking-wide text-text select-none">
       ankush<span class="inline-block text-accent animate-pulse">&nbsp;.</span>
     </h1>
+    <router-link
+      to="/blog"
+      class="hidden md:inline-block mt-4 px-6 py-2 text-sm font-medium tracking-wide text-text-secondary border border-nav-border rounded-lg hover:text-accent hover:border-accent hover:bg-accent/10 transition-colors"
+    >
+      blog
+    </router-link>
   </div>
 </template>
